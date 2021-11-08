16436910b1c70b15fa11a271a955b24eb5d7d2bc
daw2-castillo22 <91275860+daw2-castillo22@users.noreply.github.com>
2021-11-08 18:26:12 +0100

    Add files via upload

---
+++ Exercici2Prac6DanielCastilloFormProcess.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+ </head>
+ <body>
+	<form action="" method="post">
+		<select name="selector">
+			<option value="1"><?php echo $_POST['input1'];?></option>
+			<option value="2"><?php echo $_POST['input2'];?></option>
+			<option value="3"><?php echo $_POST['input3'];?></option>
+			<option value="4"><?php echo $_POST['input4'];?></option>
+			<option value="5"><?php echo $_POST['input5'];?></option>
+		</select>
+	</form>
+ </body>
+</html>
---
+++ Exercici3Prac6DanielCastilloProcessForm.php
@@ -0,0 +1,12 @@
+<html>
+ <head>
+ </head>
+ <body>
+ <?php
+$total;
+$total = $_POST['n1']+$_POST['n2']+$_POST['n3'];
+ 
+echo "El total de la suma es: ".$total;
+?>
+ </body>
+</html>
